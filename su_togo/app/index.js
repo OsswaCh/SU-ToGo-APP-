968e9dcbbcc97e5fa2b5203137aa025cacdf63b3
MohamedOmarSabry <momo12320@aucegypt.edu>
2024-05-07 20:05:53 +0300

    Fixed the Index Page

--- su_togo/app/index.js
+++ su_togo/app/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Image, SafeAreaView, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
+import { View, StyleSheet, Image, SafeAreaView, ScrollView, TouchableOpacity, TextInput, Button, Alert, Text } from 'react-native';
 import { Link,useNavigation } from 'expo-router';
 
 export default function Page() {
@@ -24,13 +24,17 @@ export default function Page() {
             <View style={styles.headercontainer}>
                 <Image
                   source={require('../assets/student_union_logo.png')}
-                  style={[styles.titleImage, { borderRadius: 10, width: 300, height: 300 }]}
+                  style={[styles.titleImage, { borderRadius: 10, width: 300, height: 260 }]}
+                />
+                <Image
+                  source={require('../assets/frontpage.png')}
+                  style={[styles.titleImage, { borderRadius: 10, width: 250, height: 60 }]}
                 />
             </View> 
             <View style={styles.box}>
               <TouchableOpacity style={styles.link}>
                 <Link href="/store_page" style={{color: "black",fontSize: 16,fontWeight: '500',fontFamily: "Inter", width: "100%", height: "100%", alignContent:"center", textAlign: "center" }}>
-                  Store Page
+                  Start Shopping
                 </Link>
               </TouchableOpacity>
             </View>
@@ -42,6 +46,7 @@ export default function Page() {
               </TouchableOpacity>
             </View> */}
             {/* Password input */}
+            <View style={styles.password}>
             <TextInput
               style={styles.input}
               placeholder="Enter Password"
@@ -51,16 +56,20 @@ export default function Page() {
             />
 
             {/* Login button */}
-            <Button title="Admin Login" onPress={handleLogin} />
+            <TouchableOpacity onPress={handleLogin} style={{alignItems: "center", width: "50%", height: 50, backgroundColor: "#D9D9D966", borderRadius: 30, justifyContent: "center"}}>
+              <Text style={{color: "black",fontSize: 12,fontWeight: '500',fontFamily: "Inter"}}>Admin</Text>
+            </TouchableOpacity>
 
             {/* Other links */}
-            <View style={styles.box}>
+            </View>
+            
+            {/* <View style={styles.box}>
               <TouchableOpacity style={styles.link}>
                 <Link href="/admin_page" style={{color: "black",fontSize: 16,fontWeight: '500',fontFamily: "Inter", width: "100%", height: "100%", alignContent:"center", textAlign: "center" }}>
                   Manager Page
                 </Link>
               </TouchableOpacity>
-            </View>
+            </View> */}
         </SafeAreaView>
       </ScrollView>
       
@@ -90,12 +99,11 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     height: "100%",
   },
-
   headercontainer: {
-    marginBottom: 20,
+    //marginBottom: 20,
     backgroundColor: '#FFFFFF',
     width:"100%",
-    maxHeight: 240,
+    maxHeight: 330,
     alignItems: "center",
     objectFit: "fill"
   },
@@ -130,10 +138,27 @@ const styles = StyleSheet.create({
     opacity: 1,
   },
   input: {
-    height: 40,
+    color: 'black', // Link color
+    fontSize: 16, // Font size
+    fontWeight: '500', // Font weight
+    fontFamily: "Inter",
+    height: 70,
     width: '80%',
-    margin: 12,
+    margin: 20,
     borderWidth: 1,
-    padding: 10,
+    textAlign: "center",
+    borderRadius: 30,
+    //padding: 10,
+
+  },
+  password:
+  {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: 200,
+    alignItems: "center",
+    //justifyContent: "space-between"
+
   },
 });
